0af80e97246728ae10569b619a4e058bfe502f72
zsofip <91950899+zsofip@users.noreply.github.com>
2022-04-04 18:48:18 +0200

    firebase deploy done

--- myCraft/src/environments/environment.ts
+++ myCraft/src/environments/environment.ts
@@ -1,9 +1,19 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+const firebaseConfig = {
+  apiKey: "AIzaSyBJw96vMmhkTonAMJfHLA9LxJZ7I6f0I2E",
+  authDomain: "mycraft-demo.firebaseapp.com",
+  projectId: "mycraft-demo",
+  storageBucket: "mycraft-demo.appspot.com",
+  messagingSenderId: "382925946462",
+  appId: "1:382925946462:web:84bbb4fb0aac58228f3ecf"
+};
 
 export const environment = {
   production: false,
+
+  firebaseConfig,
 
   apiUrl: 'https://nettuts.hu/jms/pzsofi/'
 };
